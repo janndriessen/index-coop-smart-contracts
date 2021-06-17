5864696d8976286a06c90ef68a70e59df264d438
JD <jann.driessen@gmx.de>
2021-06-17 21:56:22 +0200

    Add checking balance of comp. Update reinvest function

--- contracts/adapters/CompReinvestmentAdapter.sol
+++ contracts/adapters/CompReinvestmentAdapter.sol
@@ -2,6 +2,7 @@
 pragma solidity 0.6.10;
 
 import { Address } from "@openzeppelin/contracts/utils/Address.sol";
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import { SafeMath } from "@openzeppelin/contracts/math/SafeMath.sol";
 
 import { BaseAdapter } from "../lib/BaseAdapter.sol";
@@ -11,9 +12,11 @@ import { ISetToken } from "../interfaces/ISetToken.sol";
 import { PreciseUnitMath } from "../lib/PreciseUnitMath.sol";
 
 contract CompReinvestmentAdapter is BaseAdapter {
-  // using Address for address;
-  // using PreciseUnitMath for uint256;
-  // using SafeMath for uint256;
+  using Address for address;
+  using PreciseUnitMath for uint256;
+  using SafeMath for uint256;
+
+  address public constant comp = address(0xc00e94Cb662C3520282E6f5717214004A7f26888);
 
   /* ============ State Variables ============ */
 
@@ -36,19 +39,30 @@ contract CompReinvestmentAdapter is BaseAdapter {
   /* ============ External Functions ============ */
 
   function reinvest() external onlyOperator {
-    // TODO: check comp balance, return if < 1
-    // TODO: claim comp
+    uint256 claimableAmount = _claimableComp();
+    // TODO: specify minimum amount to claim?
+    require(claimableAmount > 1, "Reinvestment failed - not enough COMP to claim");
+    _claimComp();
+    _sellComp();
     // TODO: reinvest for cETH?
   }
 
   /* ============ Internal Functions ============ */
 
-  function _claimableComp() internal view returns(uint256) {}
+  function _claimableComp() internal view returns(uint256) {
+    // TODO: get correct address?
+    uint256 balance = IERC20(comp).balanceOf(address(this));
+    return balance;
+  }
 
   function _claimComp() internal {
     // TODO: get correct address
     comptroller.claimComp(address(this));
   }
 
-  function _sellComp() internal {}
+  function _sellComp() internal {
+    uint256 _comp = IERC20(comp).balanceOf(address(this));
+    // TODO: sell in exchange for what token?
+    // TODO: sell via uni/sushi?
+  }
 }
